333a329c33464b38903ecc0364b628aafa1e4f57
Cleber Belisse <124924550+cbelisse@users.noreply.github.com>
2023-05-19 16:44:49 -0300

    28 - Aula 28 (19-05-2023)
    Aula 3 de PHP

---
+++ 28 - Aula 28 (19-05-2023)/php/rodape.php
@@ -0,0 +1,14 @@
+<?php
+    echo "
+    <!--Rodapé-->
+    <div class='container-fluid'>
+        <p><a href=''>BELISSE CORPORATE Design for Web</a> | Copyright © 2023.</p>
+
+    </div>
+
+
+    
+</body>
+</html>
+"
+?>
